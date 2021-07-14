349860d7848260a504e3f0cc1e0da728e755390e
AimadBachar <aimadbaba@hotmail.fr>
2021-07-14 15:11:49 +0200

    component header index.js scss mobile

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 
 //import PropTypes from 'prop-types';
 
+import appBanner from '/src/assets/images/app-covoiturage.png';
+import login from '/src/assets/images/login.png';
+import menu from '/src/assets/images/menu.png';
+//import 'semantic-ui-css/semantic.min.css';
+import './styles.scss';
+
 const Header = () => (
-<div>
-  coucou je suis dans le header
-</div>
+  <div className="header">
+    <img className="header-logo" src={appBanner} alt="appBanner" />
+    <img className="header-login" src={login} alt="login" />
+    <img className="header-menu" src={menu} alt="menu" />
+  </div>
+
 );
 
 // Header.proptypes = {
