110d30886a12ece0479ccd3c27e99ade7870e594
AimadBachar <aimadbaba@hotmail.fr>
2021-07-14 22:11:54 +0200

    footer css to the end fixed

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -27,11 +27,11 @@ const Card = () => (
       <div className="card-top">
         <div className="card-top_left">
           <img className="card-like" src={iconLike} alt="icon-like" />
-          <a className="card-profil">Laurent</a>
+          <a className="card-profil">Aimad</a>
         </div>
         <div className="card-top_right">
-          <p className="card-date">14/07/2021</p>
-          <p className="card-hour">12h45</p>
+          <p className="card-date">16/07/2021</p>
+          <p className="card-hour">11h45</p>
         </div>
       </div>
       <p className="card-destination">Bordeaux -> Quibééééron</p>
@@ -39,7 +39,41 @@ const Card = () => (
         <span className="card-tag">SURF</span>
         <button className="card-button" type="button">GO !</button>
       </div>
-    </div> 
+    </div>
+    <div className="card">
+      <div className="card-top">
+        <div className="card-top_left">
+          <img className="card-like" src={iconLike} alt="icon-like" />
+          <a className="card-profil">Anna</a>
+        </div>
+        <div className="card-top_right">
+          <p className="card-date">16/07/2021</p>
+          <p className="card-hour">18h45</p>
+        </div>
+      </div>
+      <p className="card-destination">Bordeaux -> Quibééééron</p>
+      <div className="card-bottom">
+        <span className="card-tag">KITESURF</span>
+        <button className="card-button" type="button">GO !</button>
+      </div>
+    </div>
+    <div className="card">
+      <div className="card-top">
+        <div className="card-top_left">
+          <img className="card-like" src={iconLike} alt="icon-like" />
+          <a className="card-profil">Anna</a>
+        </div>
+        <div className="card-top_right">
+          <p className="card-date">16/07/2021</p>
+          <p className="card-hour">18h45</p>
+        </div>
+      </div>
+      <p className="card-destination">Bordeaux -> Quibééééron</p>
+      <div className="card-bottom">
+        <span className="card-tag">SKATE</span>
+        <button className="card-button" type="button">GO !</button>
+      </div>
+    </div>
   </div>
 );
 
