6890e0e4fd0783becdfd8fd4473a8e23fff5bdff
Laurent Savarit <laurent.savarit@laposte.net>
2021-07-14 11:38:20 +0200

    check method findOne for User model

--- api/app/models/coreModel.js
+++ api/app/models/coreModel.js
@@ -11,17 +11,29 @@ class coreModel {
     static async findAll(){
 
         try{
-
             const sqlQuery = `SELECT * FROM "${this.tableName}";`;
-
             const {rows} = await pool.query(sqlQuery);
-    
             return rows ? rows.map(row=>new this(row)) : new Error("internal error");
 
         }catch(err){
             throw err.message;
         }
+    };
 
+    static async findOne(id){
+
+        try{
+            const sqlQuery ={
+                text: `SELECT * FROM "${this.tableName}" WHERE id = $1;`,
+                values:[parseInt(id,10)]
+            };
+
+            const {rows} = await pool.query(sqlQuery);
+            return rows[0] ? new this(rows[0]) : new Error(`id ${id} not found...`);
+
+        }catch(err){
+            throw err.message;
+        }
     }
 
 
--- api/test/models.js
+++ api/test/models.js
@@ -1,25 +1,47 @@
 const User = require("../app/models/User");
-const {expect} = require("chai");
+const {
+    expect
+} = require("chai");
 
-describe("User model", function(){
-    describe("#getAll()", async function(){
+
+//tests unitaires pour le model User
+describe("User model", function () {
+    describe("#findAll()", async function () {
 
         let results;
 
-        before( async function(){
-          results = await User.findAll();  
+        before(async function () {
+            results = await User.findAll();
         })
-        
 
-        it("should return an array", function(){
+
+        it("should return an array", function () {
             expect(results).to.be.a("array");
         });
-        it("should return an object in array", function(){
+        it("should return an object in array", function () {
             expect(results[0]).to.be.a("object");
         });
-        it("should return an instance of User", function(){
+        it("should return an instance of User", function () {
             expect(results[0] instanceof User).to.be.equal(true);
         })
-    })
-})
+    });
 
+    describe("#findOne(id)", async function () {
+
+        let result;
+
+        const id = 3;
+
+        before(async function () {
+            result = await User.findOne(id);
+        })
+
+        it("should return an object", function () {
+            expect(result).to.be.a("object");
+        });
+        it("should return an instance of User", function () {
+            expect(result instanceof User).to.be.equal(true);
+        })
+    })
+
+})
