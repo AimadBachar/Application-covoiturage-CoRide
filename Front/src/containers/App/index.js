e0b8491a9ea2a6b24f5b861a8215380723ea007e
JulienGuillerm <julien.guillerm1@gmail.com>
2021-07-17 11:28:06 +0200

    folders containers

---
+++ Front/src/containers/App/index.js
@@ -0,0 +1,18 @@
+// import { connect } from 'react-redux';
+// import App from 'src/components/App';
+// import { fetchRecipes } from 'src/actions/recipes';
+
+// const mapStateToProps = (state) => ({
+//   loading: state.recipes.loading
+// });
+
+// const mapDispatchToProps = (dispatch) => ({
+//   fetchRecipes: () => {
+//     console.log('aller chercher les recettes');
+
+//     const action = fetchRecipes();
+//     dispatch(action);
+//   }
+// });
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
