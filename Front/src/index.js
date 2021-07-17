9fd022ce749037083b8744d8e8cc31adf1263d02
JulienGuillerm <julien.guillerm1@gmail.com>
2021-07-17 11:22:05 +0200

    folder reducers/store/containers

--- Front/src/index.js
+++ Front/src/index.js
@@ -1,15 +1,26 @@
 // == Import : npm
 import React from 'react';
+// import ReactDom from 'react-dom';
 import { render } from 'react-dom';
+// import { Provider } from 'react-redux';
+// import App from 'src/containers/App';
+// import { BrowserRouter as Router } from 'react-router-dom';
 
 // == Import : local
 // Composants
 import App from 'src/components/App';
+// import store from 'src/store'; // Vient de redux
 
 // == Render
 // 1. Élément React racine (celui qui contient l'ensemble de l'app)
 //    => crée une structure d'objets imbriqués (DOM virtuel)
-const rootReactElement = <App />;
+const rootReactElement = (
+//   <Provider store={store}>
+// <Router>
+      <App />
+// </Router>
+// </Provider>
+);
 // 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
 const target = document.getElementById('root');
 // 3. Déclenchement du rendu de React (virtuel) => DOM (page web)
