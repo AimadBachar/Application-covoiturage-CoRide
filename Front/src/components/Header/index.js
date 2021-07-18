244d9a66273ede0ecd396372601f4982797c308d
JulienGuillerm <julien.guillerm1@gmail.com>
2021-07-18 10:46:11 +0200

    resolve-conflict-git

--- Front/src/components/Header/index.js
+++ Front/src/components/Header/index.js
@@ -12,23 +12,6 @@ const Header = (props) => (
   <div className="header">
     <img className="header-logo" src={appBanner} alt="appBanner" />
 
-    <div className="nav" onClick={props.onButtonClickMenu}>
-      <nav role="navigation">
-        <div id="menuToggle">
-          <input type="checkbox" />
-          <span />
-          <span />
-          <span />
-          <ul id="menu">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Info</a></li>
-            <li><a href="#">Contact</a> </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-
     <Link
       to="/LoginForm"
       exact
