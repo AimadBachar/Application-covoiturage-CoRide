9e7acb5ac21ed7cbaf68b65d3f5ca9a18b2e36ad
AimadBachar <aimadbaba@hotmail.fr>
2021-07-21 19:24:53 +0200

    Component-card-add-card.driver-and-travel-id

--- Front/src/components/Card/index.js
+++ Front/src/components/Card/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 
-import iconLike from '/src/assets/images/pouce-en-l_air.png';
+//import iconLike from '/src/assets/images/pouce-en-l_air.png';
 import './styles.scss';
 
 const Card = ({
@@ -13,11 +13,11 @@ const Card = ({
   <div>
     {console.log("compo cars", cards)}
     {cards.map((card) => (
-      <div className="card" key={card.id}>
+      <div className="card" key={card.travel_id}>
         <div className="card-top">
           <div className="card-top_left">
-            <img className="card-like" src={iconLike} alt="icon-like" />
-            <a href="#" className="card-profil" onClick={onButtonClickProfilUser}>Pseudo</a>
+            {/* <img className="card-like" src={iconLike} alt="icon-like" /> */}
+            <a href="#" className="card-profil" onClick={onButtonClickProfilUser}>{card.driver}</a>
           </div>
           <div className="card-top_right">
             <p className="card-date">{card.departure_timestamp}</p>
@@ -27,7 +27,7 @@ const Card = ({
         <p className="card-destination">Départ: {card.departure_city}</p>
         <p className="card-destination">Arrivée: {card.destination_city}</p>
         <div className="card-bottom">
-          <span className="card-tag">{card.tag}</span>
+          <span className="card-tag">{card.activity}</span>
           <button className="card-button" type="button" onClick={onButtonClickValidation}>GO !</button>
         </div>
       </div>
