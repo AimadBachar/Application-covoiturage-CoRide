5200e97909d57af1cd477ace0281d295d487d8bc
JulienGuillerm <julien.guillerm1@gmail.com>
2021-07-24 11:54:46 +0200

    card-css-done

--- Front/src/components/Card/index.js
+++ Front/src/components/Card/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom'
 
 //import iconLike from '/src/assets/images/pouce-en-l_air.png';
 import './styles.scss';
@@ -13,24 +14,30 @@ const Card = ({
   <div>
     {console.log("compo cards", cards)}
     {cards.map((card) => (
-      <div className="card" key={card.travel_id}>
-        <div className="card-left">
-          <div className="card-left_travel">
+      <Link
+      exact
+      to="#"
+      >
+      <div className="cardInfos" key={card.travel_id}>
+        <div className="cardInfos-travel">
+          <div className="cardInfos-travel_left">
             {/* <img className="card-like" src={iconLike} alt="icon-like" /> */}
-            <a href="#" className="card-profil" onClick={onButtonClickProfilUser}>{card.driver}</a>
-            <p className="card-destination">Départ: {card.departure_city}</p>
-            <p className="card-destination">Arrivée: {card.destination_city}</p>
+            <a href="#" className="cardInfos-travel_left__profil" onClick={onButtonClickProfilUser}>{card.driver}</a>
+            <p className="cardInfos-travel_left__destination">{card.departure_city}</p>
+            <p className="cardInfos-travel_left__destination">{card.destination_city}</p>
           </div>
-          <div className="card-left_infos">
-             <p className="card-date">{new Date(card.departure_timestamp).toLocaleDateString("fr-FR")}</p> 
-            <p className="card-hour">{new Date(card.departure_timestamp).getUTCHours()}h{new Date(card.departure_timestamp).getUTCMinutes()}</p>
+          <div className="cardInfos-travel_right">
+            <p className="cardInfos-travel_right__date">{new Date(card.departure_timestamp).toLocaleDateString("fr-FR")}</p> 
+            <p className="cardInfos-travel_right__hour">{new Date(card.departure_timestamp).getUTCHours()}h{new Date(card.departure_timestamp).getUTCMinutes()}</p>
+          
+                  {/* <div className="card-right"> */}
+            <span className="cardInfos-travel_right__tag">{card.activity}</span>
           </div>
         </div>
-        {/* <div className="card-right"> */}
-          <span className="card-tag">{card.activity}</span>
-          <button className="card-button" type="button" onClick={onButtonClickValidation}>GO !</button>
-        {/* </div> */}
+                  {/* <button className="card-button" type="button" onClick={onButtonClickValidation}>GO !</button> */}
+                {/* </div> */}
       </div>
+      </Link>
     ))}
   </div>
 );
