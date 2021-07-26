4e8066048f16522f7a6c95f9464f6946d6273e5f
Etudiant O'clock <contact.annasion@gmail.com>
2021-07-26 17:56:57 +0200

    profil-user-import-reducers

--- Front/src/reducers/index.js
+++ Front/src/reducers/index.js
@@ -1,13 +1,16 @@
+// ==> Import des Reducers
 import { combineReducers } from 'redux';
 
-import trajetsReducer from './trajets';
+import trajetsReducer from './trajets.js';
 import userReducer from 'src/reducers/user.js';
 import usersigninReducer from 'src/reducers/usersignin.js';
+import profiluserReducer from 'src/reducers/profiluser.js';
 
 const rootReducer = combineReducers({
   user: userReducer,
   usersignin: usersigninReducer,
   trajets: trajetsReducer,
+  profiluser: profiluserReducer,
 });
 
 export default rootReducer;
