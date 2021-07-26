44adcfa1c7179885bf64fbb8ebcce04a06febf4b
Etudiant O'clock <contact.annasion@gmail.com>
2021-07-26 22:11:11 +0200

    profiluser-delete-to-do-it-again

--- Front/src/reducers/index.js
+++ Front/src/reducers/index.js
@@ -4,13 +4,13 @@ import { combineReducers } from 'redux';
 import trajetsReducer from './trajets.js';
 import userReducer from 'src/reducers/user.js';
 import usersigninReducer from 'src/reducers/usersignin.js';
-import profiluserReducer from 'src/reducers/profiluser.js';
+
 
 const rootReducer = combineReducers({
   user: userReducer,
   usersignin: usersigninReducer,
   trajets: trajetsReducer,
-  profiluser: profiluserReducer,
+  //profiluser: profiluserReducer,
 });
 
 export default rootReducer;
