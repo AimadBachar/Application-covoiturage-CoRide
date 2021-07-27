d9cf143bb1dbf4d2e12500619fdc9f8d9fe4999d
Etudiant O'clock <contact.annasion@gmail.com>
2021-07-27 11:28:40 +0200

    profil-user-axios-test

--- Front/src/reducers/userprofil.js
+++ Front/src/reducers/userprofil.js
@@ -2,6 +2,7 @@
 import {
     USER_PROFIL_INPUT_CHANGE,
     USER_PROFIL_SUCCESS,
+    USER_PROFIL_ACTIVITIES,
   } from 'src/actions/userprofil';
   import { USER_PROFIL } from 'src/actions/userprofil';
   
@@ -9,16 +10,22 @@ import {
   // j'ajoute un reducer-user.js avec de fausses datas
   // puis je modifie le state du container login avec ces fausses datas
   export const initialState = {
-    logged: false,
-    loggedMessage: 'Welcome !',
+    completed: false,
+    profilCompletedMessage: 'Profil success !',
     inputs: {
-      // mail pour tester
-      /*
-          email: 'coride-anna@app.com',
-          password: 'anna',
-         */
-      // user: 'laurent@savarit.fr',
-      // password: '1234',
+      last_name: 'sasa',
+      first_name: 'lolo',
+      pseudo: 'Lolo',
+      email: 'lolo@sasafr',
+      password: 'tatayoyo',
+      birthdate: '23/02/1989',
+      coords: 'hh',
+      city: 'ciboure',
+      postcode: '64500',
+      country: 'France',
+      brand: 'Peugeot',
+      model: 'Expert',
+      activity_id: '1',
     },
   
   };
@@ -27,22 +34,31 @@ import {
     /* console.log(action.payload); */
     switch (action.type) {
       case USER_PROFIL_SUCCESS:
+        console.log(action.payload);
         return {
           ...state,
-          profilCompletedMessage: `Profil success!`,
-          completedName: action.payload.first_name,
+          profilCompletedMessage: `Profil Success !`,
           completed: true,
           ...action.payload,
         };
 
       case USER_PROFIL_INPUT_CHANGE:
+        console.log(action.payload);
         return {
           ...state,
           inputs: {
             ...state.inputs,
-            ...action.payload,
+            [action.name] : action.payload,
           },
         };
+
+       /* case USER_PROFIL_ACTIVITIES:
+          console.log(action.payload);
+          return {
+            ...state,
+            ...action.payload
+          }*/
+
       default:
         return state;
     }
