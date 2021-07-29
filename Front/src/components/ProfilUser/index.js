692d50d06518f345ff7244f0630d76fe5bb01107
Etudiant O'clock <contact.annasion@gmail.com>
2021-07-29 08:00:31 +0200

    user-form

--- Front/src/components/ProfilUser/index.js
+++ Front/src/components/ProfilUser/index.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 
 import Field from 'src/components/ProfilUser/Field';
 import 'src/components/ProfilUser/styles.scss';
-//import photoKite from 'src/assets/images/kitewindsurf.jpg';
+import icone from 'src/assets/images/login.png';
 
 const ProfilUser = ({
 
@@ -101,26 +101,46 @@ return (
             enctype="application/x-www-form-urlencoded"
           >
             <h1 className="profil-form-title">
-            À propos de vous
+            Modifier le profil
             </h1>
-
+        <div className="profil-form-header">
+          <div className="profil-form-upper">
+            <div className="profil-form-upper_picture">
+            <img className="profil-form-upper_picture_icone" src={icone} alt="photo kite" />
+            </div>
+                <input
+                  className="profil-form-upper_upload"
+                  type="file"
+                  name="picture"
+                  placeholder="Picture"
+                  accept="image/png, image/jpeg"
+                  onChange={handleUpload}          
+                /> 
+               </div>
+              <div className="profil-form-identity">
+              <div className="profil-form-firstname">
                 <Field
-                  className="profil-form-input"
+                  className="profil-form-firstname"
                   type="text"
                   name="prénom"
                   placeholder="Prénom"
                   onChange={changeField}
                   value={first_name}
                 />
+                </div>
+                <div className="profil-form-lastname">
                 <Field
-                  className="profil-form-input"
+                  className="profil-form-lastname"
                   type="text"
                   name="nom"
                   placeholder="Nom"
                   onChange={changeField}
                   value={last_name}
                 />
-
+                </div>
+                </div>
+                 </div>
+                 <div className="profil-form-pseudobirthdate">
                 <Field
                   className="profil-form-input"
                   type="text"
@@ -129,39 +149,123 @@ return (
                   onChange={changeField}
                   value={pseudo}
                 />
-
+               
                 <input
-                  className="signin-form-input"
-                  type="file"
-                  name="picture"
-                  placeholder="Picture"
-                  accept="image/png, image/jpeg"
-                  onChange={handleUpload}
-                //TODO style du champ file à faire           
-                />
-
-                <Field
-                  className="profil-form-input-date"
+                  className="profil-form-date"
                   type="date"
                   name="birthdate"
                   placeholder="Date de naissance"
-                  //autocorrect="off"
+                  autocorrect="off"
                   data-date-split-input="true"
                   onChange={changeField}
                   value={birthdate}
                 />
-
-                <Field
+              </div>
+              
+              <div className="profil-form-sport">
+                <select
+                  className="profil-form-sport_select"
+                  type="select"
+                  name="activity_id"
+                  value={activity_id}
+                  onChange={changeField}
+                >
+                  <option
+                  className="profil-form-sport_title" >Choisissez votre sport passion n°1</option>
+                 { tags.map((tag) => (
+                    <option
+                      name="tag"
+                      key={tag.id}
+                      value={tag.id}
+                    >
+                      {tag.label}
+                    </option>
+                  ))}    
+                </select>
+            <div className="profil-form-sport_others">       
+            <input type="text" className="profil-form-sport_input" placeholder="Sport passion n°2"></input>    
+            <input type="text" className="profil-form-sport_input" placeholder="Sport passion n°3"></input>
+              </div>                
+              </div> 
+              <div className="profil-form-bio">
+               
+                <textarea className="profil-form-textarea" 
+                cols="20" rows="5" wrap="hard" placeholder="plus d'informations sur vous, vos spots préférés"></textarea>
+              </div>
+              <div className="profil-form-emailpassword">
+              <Field
                   className="profil-form-input"
                   type="email"
                   name="email"
-                  placeholder="Adresse Email" 
+                  placeholder="E-mail" 
                   onChange={changeField}
                   value={email}
                 />
 
-           
-{/*}
+              <Field
+                  className="profil-form-input"
+                  type="password"
+                  name="password"
+                  placeholder="Mot de passe"
+                  onChange={changeField}
+                  value={password}
+                />
+                </div>
+              <div className="profil-form-button">
+                <button 
+                type="submit" 
+                className="profil-form-submit"
+                >
+                  Sauvegarder
+                </button>
+              </div>
+          </form>
+        )}
+              <div className="home-redirection">
+                <p className="home-redirection-text">
+                    Retour sur la        
+                  <Link
+                    className="home-redirection-link"
+                    to="/"
+                  >
+                  page d'accueil
+                  </Link>
+                </p>
+              </div>         
+        </div>
+      </div>
+    );
+};
+
+ProfilUser.propTypes = {
+  last_name: PropTypes.string.isRequired,
+  tags: PropTypes.string,
+  first_name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  birthdate: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  coords: PropTypes.string.isRequired,
+  city: PropTypes.string.isRequired,
+  postcode: PropTypes.string.isRequired,
+  country:  PropTypes.string.isRequired,
+  activity_id: PropTypes.string.isRequired,
+  changeField: PropTypes.func.isRequired,
+  handleProfil: PropTypes.func.isRequired,
+  isCompleted: PropTypes.bool,
+  
+};
+// Valeurs par défaut pour les props
+ProfilUser.defaultProps = {
+  isCompleted: false,
+  profilCompletedMessage: 'Profil completed',
+};
+
+export default ProfilUser;
+
+
+
+// autres champs  pour le profil de l'utilisateur
+/*}
                 <Field
                   className="profil-form-input"
                   type="text"
@@ -215,95 +319,15 @@ return (
                   onChange={changeField}
                   value={model}
                 />       
-              */}
+              */
+
+                /*
+                 <p className="profil-form-text">Bio</p>
+                 */
+
+
+                 /*
+
+
                 <p className="profil-form-text">Quel(s) sport(s) de glisse pratiquez vous ?</p>
-          
-              <div className="profil-form-sport">
-                <select
-                  className="profil-form-input"
-                  type="select"
-                  name="activity_id"
-                  value={activity_id}
-                  onChange={changeField}
-                >
-                  <option
-                  className="profil-form-select_title" >Sport 1</option>
-                 { tags.map((tag) => (
-                    <option
-                      name="tag"
-                      key={tag.id}
-                      value={tag.id}
-                    >
-                      {tag.label}
-                    </option>
-                  ))}    
-                </select>
-              
-
-              </div>                
-            
-              <div className="profil-form-bio">
-                <p className="profil-form-text">Bio</p>
-                <textarea className="profil-form-textarea" cols="20" rows="5" wrap="hard"></textarea>
-              </div>
-              <Field
-                  className="profil-form-input"
-                  type="password"
-                  name="password"
-                  placeholder="Mot de passe"
-                  onChange={changeField}
-                  value={password}
-                />
-
-              <div className="profil-form-button">
-                <button 
-                type="submit" 
-                className="profil-form-submit"
-                >
-                  Sauvegarder
-                </button>
-              </div>
-       
-
-          </form>
-        )}
-              <div className="home-redirection">
-                <p className="home-redirection-text">
-                    Retour sur la        
-                  <Link
-                    className="home-redirection-link"
-                    to="/"
-                  >
-                  page d'accueil
-                  </Link>
-                </p>
-              </div>         
-        </div>
-      </div>
-    );
-};
-
-ProfilUser.propTypes = {
-  last_name: PropTypes.string.isRequired,
-  tags: PropTypes.string,
-  first_name: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-  birthdate: PropTypes.string.isRequired,
-  password: PropTypes.string.isRequired,
-  coords: PropTypes.string.isRequired,
-  city: PropTypes.string.isRequired,
-  postcode: PropTypes.string.isRequired,
-  country:  PropTypes.string.isRequired,
-  activity_id: PropTypes.string.isRequired,
-  changeField: PropTypes.func.isRequired,
-  handleProfil: PropTypes.func.isRequired,
-  isCompleted: PropTypes.bool,
-  
-};
-// Valeurs par défaut pour les props
-ProfilUser.defaultProps = {
-  isCompleted: false,
-  profilCompletedMessage: 'Profil completed',
-};
-
-export default ProfilUser;
+          */
