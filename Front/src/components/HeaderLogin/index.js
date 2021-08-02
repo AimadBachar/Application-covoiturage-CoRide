696b2ff92de6185b5e502d29191346c3437d8a1f
Etudiant O'clock <contact.annasion@gmail.com>
2021-08-02 12:25:11 +0200

    headerlogin-proptypes-indentation-done

--- Front/src/components/HeaderLogin/index.js
+++ Front/src/components/HeaderLogin/index.js
@@ -1,23 +1,22 @@
+// Import : npm
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-// import PropTypes from 'prop-types';
 
+// == Import : local
 import loginBanner from 'src/assets/images/snowbaord2.jpg';
 import logo from '/src/assets/logo/coride-b.svg';
 import login from '/src/assets/images/icon user white.png';
 import logout from '/src/assets/images/logout-white.png';
 import Nav from 'src/components/Nav';
-
-// import 'semantic-ui-css/semantic.min.css';
 import 'src/components/HeaderLogin/styles.scss';
 
-// JSON.parse(localStorage.getItem('token'))['access_token']
-
+// == Composant
 const HeaderLogin = ({
   logged,
   onButtonClickLogout
+
 }) => {
-  // let user;
   const logOut = () => {
     localStorage.clear();
     onButtonClickLogout();
@@ -28,12 +27,9 @@ const HeaderLogin = ({
       <div className="header">
         <img className="header-photo" src={loginBanner} alt="loginBanner" />
         <img src={logo} className="header-logo" alt="Logo CoRide" />
-        <Nav logged={logged}/>
-
-        <a
-            onClick={logOut}
-          >
-            <img className="header-logout" src={logout} alt="logout" />
+         <Nav logged={logged}/>
+          <a onClick={logOut}>
+           <img className="header-logout" src={logout} alt="logout" />
           </a>
       </div>
     )
@@ -43,22 +39,23 @@ const HeaderLogin = ({
         <img className="header-photo" src={loginBanner} alt="loginBanner" />
         <img src={logo} className="header-logo" alt="Logo CoRide" />
         <Nav logged={logged}/>
-        <Link
-          to="/connexion"
-          exact
-          // onClick={props.onButtonClickLogin}
-        >
+         <Link to="/connexion" exact>
           <img className="header-login" src={login} alt="login" />
         </Link>
       </div>
-  )
-  }
-      
+    )
+  };     
 };
 
-// Header.proptypes = {
+HeaderLogin.propTypes = {
+  onButtonClickLogout: PropTypes.string.isRequired,
+  logged: PropTypes.bool,
+};
 
-// };
+// Valeurs par défaut pour les props
+HeaderLogin.defaultProps = {
+  logged: false, 
+};
 
 export default HeaderLogin;
 
