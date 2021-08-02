706f5772690e33ec535d01f6aae418b446442b44
Etudiant O'clock <contact.annasion@gmail.com>
2021-08-02 12:08:31 +0200

    signin-header-proptypes-and-indentation-done

--- Front/src/components/Signin/HeaderSignin/index.js
+++ Front/src/components/Signin/HeaderSignin/index.js
@@ -1,22 +1,22 @@
+// == Import : npm
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-// import PropTypes from 'prop-types';
 
+// == Import : local
 import signinBanner from '/src/assets/images/kitesurf.jpg';
 import logo from '/src/assets/logo/coride-b.svg';
 import login from '/src/assets/images/icon user white.png';
 import logout from '/src/assets/images/logout-white.png';
 import Nav from 'src/components/Nav';
-
-
 import 'src/components/Signin/HeaderSignin/styles.scss';
 
-
+// == Composant
 const HeaderSignin = ({
   logged,
-  onButtonClickLogout
+  onButtonClickLogout,
+
 }) => {
-  // let user;
   const logOut = () => {
     localStorage.clear();
     onButtonClickLogout();
@@ -28,10 +28,7 @@ const HeaderSignin = ({
         <img className="header-photo" src={signinBanner} alt="signinBanner" />
         <img src={logo} className="header-logo" alt="Logo CoRide" />
         <Nav logged={logged}/>
-
-        <a
-            onClick={logOut}
-          >
+          <a onClick={logOut}>
             <img className="header-logout" src={logout} alt="logout" />
           </a>
       </div>
@@ -42,21 +39,23 @@ const HeaderSignin = ({
         <img className="header-photo" src={signinBanner} alt="signinBanner" />
         <img src={logo} className="header-logo" alt="Logo CoRide" />
         <Nav logged={logged}/>
-        <Link
-          to="/connexion"
-          exact
-          // onClick={props.onButtonClickLogin}
-        >
-          <img className="header-login" src={login} alt="login" />
-        </Link>
+          <Link to="/connexion" exact>
+            <img className="header-login" src={login} alt="login" />
+          </Link>
       </div>
-  )
-  }
-      
+    )
+  };     
 };
 
-// Header.proptypes = {
+HeaderSignin.propTypes = {
+  onButtonClickLogout: PropTypes.string.isRequired,
+  logged: PropTypes.bool,
+};
 
-// };
+// Valeurs par défaut pour les props
+HeaderSignin.defaultProps = {
+  logged: false, 
+};
 
+// == Export 
 export default HeaderSignin;
